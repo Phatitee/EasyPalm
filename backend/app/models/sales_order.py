1e638e30d286e88622e95f6c5a96ebe4f9ddfef1
Phatitee <chaimongcol08@gmail.com>
2025-10-16 01:25:07 +0700

    edit some method in SaleOrder

--- backend/app/models/sales_order.py
+++ backend/app/models/sales_order.py
@@ -1,17 +1,19 @@
 from app import db
+from .food_industry import FoodIndustry
 
 class SalesOrder(db.Model):
     __tablename__ = 'salesorder'
     sale_order_number = db.Column(db.String(5), primary_key=True)
+    food_industry_id = db.Column(db.String(5), db.ForeignKey('foodindustry.F_id'), nullable=False)
     s_date = db.Column(db.DateTime)
     s_total_price = db.Column(db.Float)
-    f_id = db.Column(db.String(5), db.ForeignKey('farmer.f_id'), nullable=False)
-    
-    farmer = db.relationship('Farmer', back_populates='sales_orders')
-    
-    # ลบ p_id และ s_quantity ออก แล้วเพิ่ม relationship ใหม่
-    farmer = db.relationship('Farmer', back_populates='sales_orders')
+
+
+    # relationships
+
     items = db.relationship('SalesOrderItem', back_populates='sales_order', cascade="all, delete-orphan")
+    food_industry = db.relationship('FoodIndustry', back_populates='sales_orders')
+
 
     stock_outs = db.relationship('StockTransactionOut', back_populates='sales_order')
     stock_returns = db.relationship('StockTransactionReturn', back_populates='sales_order')
@@ -19,9 +21,9 @@ class SalesOrder(db.Model):
     def to_dict(self):
         return {
             'sale_order_number': self.sale_order_number,
+            'food_industry_id': self.food_industry_id,
             's_date': self.s_date.isoformat() if self.s_date else None,
             's_total_price': self.s_total_price,
-            'farmer_name': self.farmer.f_name if self.farmer else None,
             'items': [item.to_dict() for item in self.items]
         }
     def __repr__(self):
